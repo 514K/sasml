83538832c225fa8c1869544029d5ba1daa6564e2
514K <43733430+514K@users.noreply.github.com>
2019-11-14 03:59:39 +0300

    NEW FUNCTIONAL!!!!!!!!!!!!!!!!!! ITS AWESOME 4 AM

--- sas.py
+++ sas.py
@@ -1,4 +1,4 @@
-﻿import vk_api
+import vk_api
 import time
 import random
 import requests
@@ -9,7 +9,15 @@ while True:
         id = messages["items"][0]["last_message"]["from_id"]
         body = messages["items"][0]["last_message"]["text"]
         if body.lower() == "привет":
-            vk.method("messages.send",{"peer_id": id,"message":"Ку!","random_id": random.randint(0,999999990)})
+            vk.method("messages.send",{"peer_id": id,"message":"Ку! Как жизнь?","random_id": random.randint(0,999999990)}) 
+            while True:
+                messages = vk.method("messages.getConversations", {"offset":0, "count":20, "filter":"unread"})
+                if messages["count"] >= 1:
+                    id = messages["items"][0]["last_message"]["from_id"]
+                    body = messages["items"][0]["last_message"]["text"]
+                    if body.lower() == "норм":
+                        vk.method("messages.send",{"peer_id": id,"message":"Это хорошо, я рад за тебя)","random_id": random.randint(0,999999992)})
+                    time.sleep(10)
         else:
             vk.method("messages.send",{"peer_id": id,"message":"Шо ти от мени хочешь Вася?","random_id": random.randint(0,999999998)})
-    time.sleep(1000)
+    time.sleep(10)
